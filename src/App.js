ed6d6dd9ceb765c31bc0c589c70444eb6f431751
Student <student@d17-327e-pc-10.ipa.ii.agh.edu.pl>
2026-01-17 11:41:17 +0100

    dekompozycja na komponenty

--- src/App.js
+++ src/App.js
@@ -1,39 +1,17 @@
 import './App.css';
 import {useState} from "react";
 import "milligram";
+import MovieForm from "./MovieForm";
+import MoviesList from "./MoviesList";
 
 function App() {
-    const [title, setTitle] = useState('');
-    const [year, setYear] = useState('');
     const [movies, setMovies] = useState([]);
-
-    function handleAddMovie() {
-        const newMovie = {
-            title: title,
-            year: year,
-        };
-        setMovies([...movies, newMovie]);
-        console.log(movies);
-        setTitle('');
-        setYear('')
-    }
 
     return (
         <div className="container">
             <h1>My favourite movies to watch</h1>
-            <h2>Titles</h2>
-            <ul>
-                {movies.map(movie => <li key={movie.title}>{movie.title} ({movie.year})</li>)}
-            </ul>
-            <h2>Add movie</h2>
-
-            <label>Tytuł</label>
-            <input type="text" value={title} onChange={(event) => setTitle(event.target.value)}/>
-
-            <label>Rok wydania</label>
-            <input type="text" value={year} onChange={(event) => setYear(event.target.value)}/>
-
-            <button onClick={handleAddMovie}>Dodaj film</button>
+            <MoviesList movies={movies}/>
+            <MovieForm onMovieSubmit={(movie) => setMovies([...movies, movie])}/>
         </div>
     );
 }
